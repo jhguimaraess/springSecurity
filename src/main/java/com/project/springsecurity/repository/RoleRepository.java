1a373f3832da08f09518c1379408701b98a8d924
joao guimaraes <guimaraesjoao101@gmail.com>
2026-03-08 23:27:10 -0300

    feat: repositories

--- src/main/java/com/project/springsecurity/repository/RoleRepository.java
+++ src/main/java/com/project/springsecurity/repository/RoleRepository.java
@@ -1,4 +1,9 @@
 package com.project.springsecurity.repository;
 
-public interface RoleRepository {
+import com.project.springsecurity.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
 }
--- src/main/java/com/project/springsecurity/repository/UserRepository.java
+++ src/main/java/com/project/springsecurity/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.project.springsecurity.repository;
 
-public interface UserRepository {
+import com.project.springsecurity.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
 }
