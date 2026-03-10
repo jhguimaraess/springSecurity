5e6dd7efe658a2b59515c8344e0224c401f69ee7
joao guimaraes <guimaraesjoao101@gmail.com>
2026-03-10 00:09:01 -0300

    feat: criar funcionalidade de criar posts

---
+++ src/main/java/com/project/springsecurity/controller/PostController.java
@@ -0,0 +1,40 @@
+package com.project.springsecurity.controller;
+
+import com.project.springsecurity.controller.dto.CreatePostDto;
+import com.project.springsecurity.entities.Post;
+import com.project.springsecurity.repository.PostRepository;
+import com.project.springsecurity.repository.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+public class PostController {
+
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+
+    public PostController(PostRepository postRepository, UserRepository userRepository) {
+        this.postRepository = postRepository;
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping("/posts")
+    public ResponseEntity<Void> createPost(@RequestBody CreatePostDto dto,
+                                           JwtAuthenticationToken token){
+
+        var user = userRepository.findById(UUID.fromString(token.getName()));  //token.getName()  retorna o subject do jwt
+
+        var post = new Post();
+        post.setContent(dto.content());
+        post.setUser(user.get());
+
+        postRepository.save(post);
+
+        return ResponseEntity.ok().build();
+    }
+}
