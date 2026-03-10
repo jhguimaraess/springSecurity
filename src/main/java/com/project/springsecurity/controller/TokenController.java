db2a97a7d1def9197b0e00dd3b6bae3a172590bb
joao guimaraes <guimaraesjoao101@gmail.com>
2026-03-09 22:25:41 -0300

    feat: implementar listar todos os usuarios, apenas para administardores

--- src/main/java/com/project/springsecurity/controller/TokenController.java
+++ src/main/java/com/project/springsecurity/controller/TokenController.java
@@ -2,6 +2,7 @@ package com.project.springsecurity.controller;
 
 import com.project.springsecurity.controller.dto.LoginRequest;
 import com.project.springsecurity.controller.dto.LoginResponse;
+import com.project.springsecurity.entities.Role;
 import com.project.springsecurity.repository.UserRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
+import java.util.stream.Collectors;
 
 @RestController
 public class TokenController {
@@ -38,11 +40,18 @@ public class TokenController {
 
         var now = Instant.now();
         var expiresIn = 300L;
+
+        var scopes = user.get().getRoles()
+                .stream()
+                .map(Role::getName)
+                .collect(Collectors.joining(" "));
+
         var claims = JwtClaimsSet.builder()
                 .issuer("mybackend")
                 .subject(user.get().getUserId().toString())
                 .issuedAt(now)
                 .expiresAt(now.plusSeconds(expiresIn))
+                .claim("scope", scopes)
                 .build();
 
         var jwtValue = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
--- src/main/java/com/project/springsecurity/controller/UserController.java
+++ src/main/java/com/project/springsecurity/controller/UserController.java
@@ -9,12 +9,15 @@ import jakarta.persistence.Entity;
 import jakarta.transaction.Transactional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
 import java.util.Set;
 
 @RestController
@@ -51,5 +54,12 @@ public class UserController {
         userRepository.save(user);
 
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/users")
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
+    public ResponseEntity<List<User>> listUsers(){
+        var users = userRepository.findAll();
+        return ResponseEntity.ok(users);
     }
 }
